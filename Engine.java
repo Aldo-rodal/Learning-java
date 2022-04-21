8f56a90325a98139ce728df21fb9ec478611fb9f
Aldo-rodal <92940614+Aldo-rodal@users.noreply.github.com>
2022-04-21 16:10:59 -0400

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -2,6 +2,8 @@ package demooop;
 
 public interface Engine 
 {
+// 	An interface just show the structure of a complex code and its methods must be implemented in other class
+	
 	void changeGear(int a);
 	void speedUp(int a);
 }
